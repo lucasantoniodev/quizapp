1aea4ed3af0ef2df6d3a9630bb723c8538e329fd
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-14 19:21:55 -0300

    Componentizando widget da página quiz - Parte 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/pages/home_page.dart';
+// import 'package:quizapp/pages/home_page.dart';
+import 'package:quizapp/pages/quiz.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Quiz());
 }
 
 
---
+++ lib/pages/quiz.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({Key? key}) : super(key: key);
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Quiz curso de Flutter & Dart'),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Align(
+                  alignment: Alignment.topRight,
+                  child: Text('Pergunta 1 de 10')),
+              const Text('Pergunta'),
+              ElevatedButton(
+                onPressed: () => debugPrint('Pressionado 1'),
+                child: const Text(
+                  'Resposta 1',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+              ),
+              ElevatedButton(
+                onPressed: () => debugPrint('Pressionado 2'),
+                child: const Text(
+                  'Resposta 2',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+              ),
+              ElevatedButton(
+                onPressed: () => debugPrint('Pressionado 3'),
+                child: const Text(
+                  'Resposta 3',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+              ),
+              ElevatedButton(
+                onPressed: () => debugPrint('Pressionado 4'),
+                child: const Text(
+                  'Resposta 4',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(100, 20, 100, 20)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
